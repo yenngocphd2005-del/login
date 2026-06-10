d299ebe483e4c1340d4f7de3d73a1de61e139d6f
yenngocphd2005-del <yourgmail@gmail.com>
2026-06-10 14:22:35 +0700

    Test

--- src/test/java/com/nguyenthiyenngoc/login/LoginTest.java
+++ src/test/java/com/nguyenthiyenngoc/login/LoginTest.java
@@ -55,8 +55,8 @@ public class LoginTest {
     public void testSuccessfulLogin2() {
 
         loginPage.enterCredentials(
-                "wrong_user",
-                "wrong_password"
+                "standard_user",
+                "secret_sauce"
         );
 
         loginPage.clickLogin();
